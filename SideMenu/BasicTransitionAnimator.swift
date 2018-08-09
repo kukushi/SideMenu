377acd04f909da847d0ee6c0b0ca19fc3ec2a234
kukushi <de@sent.as>
2018-08-09 16:36:32 +0800

    Fix the compile error

--- SideMenu/BasicTransitionAnimator.swift
+++ SideMenu/BasicTransitionAnimator.swift
@@ -10,10 +10,10 @@ import UIKit
 
 // Built-In Animation Controller
 public class BasicTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
-    let animationOptions: UIView.AnimationOptions
+    let animationOptions: UIViewAnimationOptions
     let duration: TimeInterval
     
-    public init(options: UIView.AnimationOptions = .transitionCrossDissolve, duration: TimeInterval = 0.4) {
+    public init(options: UIViewAnimationOptions = .transitionCrossDissolve, duration: TimeInterval = 0.4) {
         animationOptions = options
         self.duration = duration
     }
